e6f3676582ac0200f92fbf4c2235da4775e4a6aa
1mercifull <junior1523jra@gmail.com>
2026-03-01 09:33:46 -0300

    Delete list.js

--- list.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-module.exports = (req, res)=>{
-  const db = JSON.parse(fs.readFileSync('./db.json','utf-8'));
-  const arr = Object.values(db).filter(i=> i.pago);
-  res.json(arr);
-};
