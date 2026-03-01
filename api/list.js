fa96edf322e03a4bb8b384e05e4113c939c7b6e4
1mercifull <junior1523jra@gmail.com>
2026-03-01 10:31:05 -0300

    Refactor list handler to return empty APKs list
    Replaced the previous implementation with a new handler that returns an empty list.

--- api/list.js
+++ api/list.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-module.exports = (req, res)=>{
-  const db = JSON.parse(fs.readFileSync('./db.json','utf-8'));
-  const arr = Object.values(db).filter(i=> i.pago);
-  res.json(arr);
-};
+export default function handler(req, res) {
+  res.json({ apks: [] });   // nada salvo, lista sempre vazia
+}
